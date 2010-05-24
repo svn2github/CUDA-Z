0710a2db8cbfee651712e01986ab649ec94eed82
andrew_golovnia <andrew_golovnia@5bddba1b-2737-4a35-b518-cf759745d68a>
2010-05-24 08:20:30 +0000

    Improving Mac support...
    git-svn-id: https://svn.code.sf.net/p/cuda-z/code/trunk@127 5bddba1b-2737-4a35-b518-cf759745d68a

--- pkg-macosx.sh
+++ pkg-macosx.sh
@@ -18,16 +18,14 @@ czQtGuiResPath=$czQtPath/QtGui.framework/Resources
 czVerFile=src/version.h
 czBldFile=src/build.h
 
-#install_name_tool -change @rpath/libcudart.dylib @executable_path/libcudart.dylib bin/CUDA-Z.app/Contents/MacOS/CUDA-Z
-#install_name_tool -change @rpath/libcudart.dylib @executable_path/libcudart.dylib bin/CUDA-Z.app/Contents/MacOS/libcudart.dylib
-#install_name_tool -change @rpath/libtlshook.dylib @executable_path/libtlshook.dylib bin/CUDA-Z.app/Contents/MacOS/libtlshook.dylib
-
 for lib in $czAppLibs
 do
 	cp $czLibPath/$lib $czAppBinPath
 	install_name_tool -change @rpath/$lib @executable_path/$lib $czAppBinPath/$lib
 	install_name_tool -change @rpath/$lib @executable_path/$lib $czAppBinPath/$czAppName
+	strip $czAppBinPath/$czAppName
 done
+strip $czAppBinPath/$czAppName
 
 #Add copy of qt_menu.nib to Resource subdirectory!
 cp -R $czQtGuiResPath/qt_menu.nib $czAppResPath
@@ -66,22 +64,20 @@ sudo -v
 dmgSize=`du -sk $czAppDir | tr "\t" " " | sed -e 's/ .*$//'`
 dmgSize=$((${dmgSize}/1000+1))
 hdiutil create "$tmpDmg" -megabytes ${dmgSize} -ov -type UDIF
-#dmgDisk=`hdid "$tmpDmg" | sed -ne ' /Apple_partition_scheme/ s|^/dev/([^ ]*).*$|1|p'`
 dmgDisk=`hdid -nomount "$tmpDmg" |awk '/scheme/ {print substr ($1, 6, length)}'`
 newfs_hfs -v "$outVol" /dev/r${dmgDisk}s1
 hdiutil eject $dmgDisk
 
 hdid "$tmpDmg"
-#sudo cp -Rv "$czAppDir" "/Volumes/$outVol"
 mkdir "/Volumes/$outVol/$czAppName.app"
 sudo ditto -rsrcFork -v "$czAppDir" "/Volumes/$outVol/$czAppName.app"
 sudo chflags -R nouchg,noschg "/Volumes/$outVol/*"
 ls -l "/Volumes/$outVol"
-#sudo ditto -rsrcFork -v "$czAppDir" "/Volumes/$outVol"
 hdiutil eject $dmgDisk
 
 rm -f "$outFile"
-hdiutil convert "$tmpDmg" -format UDZO -o "$outFile"
+hdiutil convert "$tmpDmg" -format UDBZ -imagekey bzip2-level=9 -o "$outFile"
+#hdiutil convert "$tmpDmg" -format UDZO -imagekey zlib-level=9 -o "$outFile"
 rm -f "$tmpDmg"
 
 exit 0
