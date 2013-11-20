182e75eb13b4f00bac314475357693cdbaac6607
andrew_golovnia <andrew_golovnia@5bddba1b-2737-4a35-b518-cf759745d68a>
2013-11-20 08:09:00 +0000

    Switch CUDA-Z to UPX packet solution on Linux.
    git-svn-id: https://svn.code.sf.net/p/cuda-z/code/trunk@205 5bddba1b-2737-4a35-b518-cf759745d68a

--- bld/bin/pkg-linux.sh
+++ bld/bin/pkg-linux.sh
@@ -7,56 +7,14 @@
 
 #set -x
 
-czPkgHeader="bld/bin/pkg-linux.dat"
-
-# Script $czPkgHeader expects variables:
-# - czNameShort - short name of cuda-z
-# - czVersion - version of cuda-z
-# - czPackSuffix - packer suffix: "gz"
-# - czExeName - name of of cuda-z
-# - czExeSize - size of packed cuda-z
-# - czExeSum - CRC of cuda-z
-# - czDllName - name of libcudart.so.*
-# - czDllSize - size of packed libcudart.so.*
-# - czDllSum - CRC of libcudart.so.*
-# Order of files in package: 1 - cuda-z.gz, 2 - libcudart.so.*
-
-czPackSuffix="gz" # Let's assume gzip is more common than bzip2...
 czExeName="cuda-z"
 czExeDir="bin"
-czDllName= #libcudart.so.4 # check this with ldd!
-czDllDir="/usr/local/cuda/lib"
+czUpx="bld/bin/upx-linux"
 
 czVerFile="src/version.h"
 czBldFile="src/build.h"
 
-if [ -z "$czDllName" ] || [ -z "$czDllDir" ]; then
-	czDll=`LANG=C ldd "$czExeDir/$czExeName" | grep -i cudart | sed -e "s/^.*=> *//" -e "s/ *(.*$//"`
-	if [ x"$czDll" = x"not found" ] && [ -n "$czDllDir" ]; then
-		czDllName=`LANG=C ldd  "$czExeDir/$czExeName" | grep -i cudart | sed -e "s/^[ \t]*//" -e "s/ *=>.*$//"`
-		czDll="$czDllDir/$czDllName"
-	fi
-	if [ -f "$czDll" ]; then
-		czDllName=`basename "$czDll"`
-		czDllDir=`dirname "$czDll"`
-	else
-		echo "Can't find CUDART library!"
-		exit 1
-	fi
-fi
-
 strip "$czExeDir/$czExeName"
-czExeSum=`md5sum "$czExeDir/$czExeName" | sed -e "s/ .*$//"`
-if [ -z "$czExeSum" ]; then
-	echo "Can't get md5 sum of $czExeDir/$czExeName!"
-	exit 1
-fi
-
-czDllSum=`md5sum "$czDllDir/$czDllName" | sed -e "s/ .*$//"`
-if [ -z "$czDllSum" ]; then
-	echo "Can't get md5 sum of $czDllDir/$czDllName!"
-	exit 1
-fi
 
 czVerMajor=`cat "$czVerFile" | grep "^#define CZ_VER_MAJOR" | sed -e "s/^.*MAJOR//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
 if [ -z "$czVerMajor" ]; then
@@ -97,81 +55,9 @@ else
 	outFile="$czNameShort-$czVersion.run"
 fi
 
-tmpDir="$HOME/tmp"
-randID=`head -c8 /dev/random | od -x | head -n1 | sed -e "s/^[0-9]* //g" | tr " " -`
-outDir="$tmpDir/$czNameShort-$randID"
+rm -f "$outFile"
 
-if [ ! -d "$tmpDir" ]; then
-	mkdir "$tmpDir"
-	sync
-	rmTmpDir=1
-fi
-mkdir "$outDir"
-if [ ! -d "$outDir" ]; then
-	echo "Cannot create temp directory."
-	if [ -z "$rmTmpDir" ]; then
-		rm -Rf "$tmpDir"
-	fi
-	exit 1
-fi
-
-case "$czPackSuffix" in
-gz)	czPack="gzip -9c" ;;
-bz2)	czPack="bzip2 -9c" ;;
-*)	echo "Unknown packer suffix!"
-	rm -Rf "$outDir"
-	if [ -z x"$rmTmpDir" ]; then
-		rm -Rf "$tmpDir"
-	fi
-	exit 1
-esac
-
-$czPack "$czExeDir/$czExeName" > "$outDir/$czExeName.$czPackSuffix"
-czExeSize=`stat -c%s "$outDir/$czExeName.$czPackSuffix"`
-if [ -z "$czExeSize" ]; then
-	echo "Can't get \$czExeSize!"
-	rm -Rf "$outDir"
-	if [ -z "$rmTmpDir" ]; then
-		rm -Rf "$tmpDir"
-	fi
-	exit 1
-fi
-
-$czPack "$czDllDir/$czDllName" > "$outDir/$czDllName.$czPackSuffix"
-czDllSize=`stat -c%s $outDir/$czDllName.$czPackSuffix`
-if [ -z "$czDllSize" ]; then
-	echo "Can't get \$czDllSize!"
-	rm -Rf "$outDir"
-	if [ -z"$rmTmpDir" ]; then
-		rm -Rf "$tmpDir"
-	fi
-	exit 1
-fi
-
-echo "#!/bin/sh" >"$outFile"
-echo "czNameShort=\"$czNameShort\"" >>"$outFile"
-echo "czVersion=\"$czVersion\"" >>"$outFile"
-echo "czPackSuffix=\"$czPackSuffix\"" >>"$outFile"
-echo "czExeName=\"$czExeName\"" >>"$outFile"
-echo "czExeSize=\"$czExeSize\"" >>"$outFile"
-echo "czExeSum=\"$czExeSum\"" >>"$outFile"
-echo "czDllName=\"$czDllName\"" >>"$outFile"
-echo "czDllSize=\"$czDllSize\"" >>"$outFile"
-echo "czDllSum=\"$czDllSum\"" >>"$outFile"
-
-cat "$czPkgHeader" >>"$outFile"
-
-echo "exit 0" >>"$outFile"
-echo "# End of code" >>"$outFile"
-
-cat "$outDir/$czExeName.$czPackSuffix" >>"$outFile"
-cat "$outDir/$czDllName.$czPackSuffix" >>"$outFile"
-
+$czUpx --ultra-brute -o"$outFile" "$czExeDir/$czExeName"
 chmod +x "$outFile"
-
-rm -Rf "$outDir"
-if [ -z "$rmTmpDir" ]; then
-	rm -Rf "$tmpDir"
-fi
 
 exit 0
