e81d9e15a3c5d23da30400d9056239f113cbad03
andrew_golovnia <andrew_golovnia@5bddba1b-2737-4a35-b518-cf759745d68a>
2014-07-07 19:57:27 +0000

    Linux shadow build is enabled
    git-svn-id: https://svn.code.sf.net/p/cuda-z/code/trunk@227 5bddba1b-2737-4a35-b518-cf759745d68a

--- bld/bin/pkg-linux.sh
+++ bld/bin/pkg-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #	\file pkg-linux.sh
-#	\brief linux package generator.
+#	\brief Linux package generator.
 #	\author Andriy Golovnya <andriy.golovnya@gmail.com> http://redscorp.net/
 #	\url http://cuda-z.sf.net/ http://sf.net/projects/cuda-z/
 #	\license GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -11,24 +11,45 @@ czExeName="cuda-z"
 czExeDir="bin"
 czUpx="bld/bin/upx-linux"
 
+czSourceDir=.
+czBuildDir=.
+
 czVerFile="src/version.h"
 czBldFile="src/build.h"
 
-strip "$czExeDir/$czExeName"
+if [ $# -gt 0 ]; then
+	czSourceDir="$1"
+fi
 
-czVerMajor=`cat "$czVerFile" | grep "^#define CZ_VER_MAJOR" | sed -e "s/^.*MAJOR//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
+if [ $# -gt 1 ]; then
+	czBuildDir="$2"
+fi
+
+if [ ! -r "$czSourceDir/$czVerFile" ]; then
+	echo Can\'t find $czSourceDir/$czVerFile
+	exit 1
+fi
+
+if [ ! -r "$czBuildDir/$czBldFile" ]; then
+	echo Can\'t find $czBuildDir/$czBldFile
+	exit 1
+fi
+
+strip "$czBuildDir/$czExeDir/$czExeName"
+
+czVerMajor=`cat "$czSourceDir/$czVerFile" | grep "^#define CZ_VER_MAJOR" | sed -e "s/^.*MAJOR//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
 if [ -z "$czVerMajor" ]; then
 	echo "Can't get \$czVerMajor!"
 	exit 1
 fi
 
-czVerMinor=`cat "$czVerFile" | grep "^#define CZ_VER_MINOR" | sed -e "s/^.*MINOR//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
+czVerMinor=`cat "$czSourceDir/$czVerFile" | grep "^#define CZ_VER_MINOR" | sed -e "s/^.*MINOR//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
 if [ -z "$czVerMinor" ]; then
 	echo "Can't get \$czVerMinor!"
 	exit 1
 fi
 
-czBldNum=`cat "$czBldFile" | grep "^#define CZ_VER_BUILD[^_]" | sed -e "s/^.*BUILD//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
+czBldNum=`cat "$czBuildDir/$czBldFile" | grep "^#define CZ_VER_BUILD[^_]" | sed -e "s/^.*BUILD//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g"`
 if [ -z "$czBldNum" ]; then
 	echo "Can't get \$czBldNum! Assume 0!"
 	czBldNum=0
@@ -36,13 +57,13 @@ fi
 
 czVersion="$czVerMajor.$czVerMinor.$czBldNum"
 
-czNameShort=`cat "$czVerFile" | grep "^#define CZ_NAME_SHORT" | sed -e "s/^.*SHORT//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g" -e "s/\"//g"`
+czNameShort=`cat "$czSourceDir/$czVerFile" | grep "^#define CZ_NAME_SHORT" | sed -e "s/^.*SHORT//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g" -e "s/\"//g"`
 if [ -z "$czNameShort" ]; then
 	echo "Can't get \$czNameShort!"
 	exit 1
 fi
 
-czBldState=`cat "$czVerFile" | grep "^#define CZ_VER_STATE" | sed -e "s/^.*STATE//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g" -e "s/\"//g"`
+czBldState=`cat "$czSourceDir/$czVerFile" | grep "^#define CZ_VER_STATE" | sed -e "s/^.*STATE//" -e "s,//.*$,," -e "s,/\*.*\*/,," -e "s/[ \t]//g" -e "s/\"//g"`
 if [ -z "$czBldState" ]; then
 	echo "Can't get \$czBldState! Assume empty!"
 	czBldState=""
@@ -55,9 +76,9 @@ else
 	outFile="$czNameShort-$czVersion.run"
 fi
 
-rm -f "$outFile"
+rm -f "$czBuildDir/$outFile"
 
-$czUpx --ultra-brute -o"$outFile" "$czExeDir/$czExeName"
+$czSourceDir/$czUpx --ultra-brute -o"$czBuildDir/$outFile" "$czBuildDir/$czExeDir/$czExeName"
 chmod +x "$outFile"
 
 exit 0
