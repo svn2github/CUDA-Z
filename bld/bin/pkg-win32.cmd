274df2bbc28d9507085b7d5b4aa929bb96ef10b7
andrew_golovnia <andrew_golovnia@5bddba1b-2737-4a35-b518-cf759745d68a>
2015-07-26 12:55:15 +0000

    Add x86/x64 build recognition on Windows (using cl.exe version string)
    git-svn-id: https://svn.code.sf.net/p/cuda-z/code/trunk@247 5bddba1b-2737-4a35-b518-cf759745d68a

--- bld/bin/pkg-win32.cmd
+++ bld/bin/pkg-win32.cmd
@@ -16,11 +16,24 @@ set czBuildDir=.
 set czVerFile=src\version.h
 set czBldFile=src\build.h
 
+set czBldSuffix=
+
 if not "%~1" == "" set czSourceDir="%~1"
 if not "%~2" == "" set czBuildDir="%~2"
 
 if not exist %czSourceDir%\%czVerFile% ( echo Can't find %czSourceDir%\%czVerFile% && goto END )
 if not exist %czBuildDir%\%czBldFile% ( echo Can't find %czBuildDir%\%czBldFile% && goto END )
+
+cl.exe 2> %czBuildDir%\cl.tst
+
+findstr "^Microsoft.(R).*for.80x86\>" %czBuildDir%\cl.tst > NUL
+if %ERRORLEVEL% equ 0 set czBldSuffix=32bit
+findstr "^Microsoft.(R).*for.x86\>" %czBuildDir%\cl.tst > NUL
+if %ERRORLEVEL% equ 0 set czBldSuffix=32bit
+findstr "^Microsoft.(R).*for.x64\>" %czBuildDir%\cl.tst > NUL
+if %ERRORLEVEL% equ 0 set czBldSuffix=64bit
+
+del /F %czBuildDir%\cl.tst
 
 set czVerMajor=
 for /f "tokens=3" %%i in ( 'findstr "^#define.CZ_VER_MAJOR\>" %czSourceDir%\%czVerFile%' ) do set czVerMajor=%%i
@@ -36,10 +49,14 @@ set czBldState=
 for /f "tokens=3" %%i in ( 'findstr "^#define.CZ_VER_STATE\>" %czSourceDir%\%czVerFile%' ) do set czBldState=%%i
 
 if "%czBldState%"=="" (
-	set czOutName=%czNameShort%-%czVersion%.exe
+	set czOutName=%czNameShort%-%czVersion%
 ) else (
-	set czOutName=%czNameShort%-%czVersion%-%czBldState%.exe
+	set czOutName=%czNameShort%-%czVersion%-%czBldState%
 )
+
+if not "%czBldSuffix%"=="" set czOutName=%czOutName%-%czBldSuffix%
+
+set czOutName=%czOutName%.exe
 
 cd %czBuildDir%
 del /F %czOutName%
